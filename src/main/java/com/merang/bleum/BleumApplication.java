4913747951137e8b650c36dd7250270323bdff0b
catnap421 <wlq7410@naver.com>
2021-03-25 23:23:21 +0900

    Chore: 환경 설정

---
+++ src/main/java/com/merang/bleum/BleumApplication.java
@@ -0,0 +1,13 @@
+package com.merang.bleum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BleumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merang/bleum/BleumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merang.bleum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BleumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
