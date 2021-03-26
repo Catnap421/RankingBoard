e350d71b7392d9fc3c637633f3b1b33f5e116417
catnap421 <wlq7410@naver.com>
2021-03-26 22:54:06 +0900

    Add: BoardController - 탑다운 코딩할 예정 -

---
+++ src/main/java/com/merang/bleum/view/board/BoardController.java
@@ -0,0 +1,42 @@
+package com.merang.bleum.view.board;
+
+import com.merang.bleum.biz.board.BoardDAO;
+import com.merang.bleum.biz.board.BoardDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    @PostMapping("/insertBoard")
+    public String insertBoard(BoardDTO boardDTO, BoardDAO boardDAO){
+        boardDAO.insertBoard(boardDTO);
+        return "getBoardList";
+    }
+
+    @PostMapping("/updateBoard")
+    public String updateBoard(BoardDTO boardDTO, BoardDAO boardDAO){
+        boardDAO.updateBoard(boardDTO);
+        return "getBoardList";
+    }
+
+    @PostMapping("/deleteBoard")
+    public String deleteBoard(BoardDTO boardDTO, BoardDAO boardDAO){
+        boardDAO.deleteBoard(boardDTO);
+        return "getBoardList";
+    }
+
+    @GetMapping("/getBoard")
+    public String getBoard(BoardDTO boardDTO, BoardDAO boardDAO, Model model){
+        model.addAttribute("board", boardDAO.getBoard(boardDTO));
+        return "getBoard";
+    }
+
+    @GetMapping("/getBoardList")
+    public String getBoardList(BoardDTO boardDTO, BoardDAO boardDAO, Model model){
+        model.addAttribute("boardList", boardDAO.getBoardList(boardDTO));
+        return "getBoardList";
+    }
+}
---
+++ src/main/java/com/merang/bleum/view/user/UserController.java
@@ -0,0 +1,8 @@
+package com.merang.bleum.view.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
